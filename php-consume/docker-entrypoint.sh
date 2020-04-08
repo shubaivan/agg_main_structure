b943e4b78119d197e73aa12a62e99e12c2b02a0e
Anmol Nagpal <4303310+anmolnagpal@users.noreply.github.com>
2020-04-08 11:59:53 -0400

    Update docker-entrypoint.sh

--- php-consume/docker-entrypoint.sh
+++ php-consume/docker-entrypoint.sh
@@ -1,13 +1,3 @@
-[program:php-fpm]
-command=php-fpm -F
-user = root
-numprocs=1
-autostart=true
-autorestart=true
-startsecs=0
-redirect_stderr=true
-stdout_logfile=/dev/stdout
-stdout_logfile_maxbytes=0
 
 echo run supervisord
 /usr/bin/supervisord
